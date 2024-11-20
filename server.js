b666b3acd56b5c631cfb66afba0a2875638ae79c
Carlos <fariascarlos2346@gmail.com>
2024-11-20 01:08:10 -0300

    ja foi no banco e retornou os usuários com os Id criados

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
     
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
     res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
     res.status(200).json(users)
 })
 
